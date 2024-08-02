c4c940a2516a0e9ecd0efbeb7886a9dc3e2214c9
SegfaultMaster <sashagoodguy@gmail.com>
2024-08-02 09:17:32 +0300

    implementing matching digit

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,8 +1,9 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
+#include <algorithm>
 
-bool match_pattern(const std::string& input_line, const std::string& pattern);
+bool match_pattern(const std::string& inputLine, const std::string& pattern);
 
 int main(int argc, char** argv) 
 {
@@ -19,19 +20,19 @@ int main(int argc, char** argv)
 
     std::string flag{argv[1]};
     std::string pattern{argv[2]};
-
+    std::cout << pattern << '\n';
     if(flag != "-E") 
     {
         std::cerr << "Expected first argument to be '-E'\n";
         return EXIT_FAILURE;
     }
 
-    std::string input_line{};
-    std::getline(std::cin, input_line);
+    std::string inputLine{};
+    std::getline(std::cin, inputLine);
     
     try 
     {
-        if(match_pattern(input_line, pattern)) 
+        if(match_pattern(inputLine, pattern)) 
         {
             return EXIT_SUCCESS;
         } 
@@ -40,19 +41,28 @@ int main(int argc, char** argv)
             return EXIT_FAILURE;
         }
     } 
-    catch(const std::runtime_error& e) 
+    catch(const std::runtime_error& error) 
     {
-        std::cerr << e.what() << '\n';
+        std::cerr << error.what() << '\n';
         return EXIT_FAILURE;
     }
     return EXIT_FAILURE;
 }
 
-bool match_pattern(const std::string& input_line, const std::string& pattern)
+bool match_pattern(const std::string& inputLine, const std::string& pattern)
 {
-    if(pattern.length() == 1) 
+    const char* patt{};
+    if(pattern == R"(\d)")
     {
-        return input_line.find(pattern) != std::string::npos;
+        return std::find_if(std::begin(inputLine), std::end(inputLine), [](const auto letter)
+        {
+            return static_cast<bool>(std::isdigit(letter));
+        }) != std::end(inputLine);
+    }
+    else if(pattern.length() == 1) 
+    {
+        
+        return inputLine.find(pattern) != std::string::npos;
     }
     else 
     {
